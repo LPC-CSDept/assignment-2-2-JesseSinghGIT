f43427c01a923dd9beebc52232de86e5862f1ce4
JesseSinghGIT <123704475+JesseSinghGIT@users.noreply.github.com>
2023-02-09 07:27:59 +0000

    program

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+
+
+    C=int(input('Enter Celsius: '))
+    F = ((9/5) * C) + 32
+    print('Fahrenheit is: {0: .2f}'.format(F))
+    # print (f"Fahrenheit is: {F: .2f}")
     pass
 
 
